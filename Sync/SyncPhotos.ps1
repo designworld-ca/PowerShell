5c60eefacbe644c53a1fa8ce13a10260877edcb5
Kevin LaRoche <kevin.laroche@designworld.ca>
2022-06-06 11:01:09 -0400

    initial commit

---
+++ Sync/SyncPhotos.ps1
@@ -0,0 +1,8 @@
+
+# sync photos from pc to plex media server
+
+$CMD = "C:\Program Files\FreeFileSync\FreeFileSync.exe"
+
+$arg1 = "D:\Documents\PersonalDocuments\Batch Jobs\BatchRun.ffs_batch"
+
+& $CMD $arg1
