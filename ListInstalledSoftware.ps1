995aa69abbd664800e3f0db90e15246230419808
Kevin LaRoche <kevin.laroche@designworld.ca>
2023-08-13 20:48:47 -0400

    updated

--- ListInstalledSoftware.ps1
+++ ListInstalledSoftware.ps1
@@ -1,3 +1,3 @@
---adjust file path as desired-includes everything but some installs are shown in non english character sets
+# adjust file path as desired-includes everything but some installs are shown in non english character sets
 
 Get-WmiObject -Class Win32_Product | Select-Object -Property Name > D:\InstalledSoftwareList.txt
